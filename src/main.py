f60daac7a930c6545c87b4070d7f68ec9d66f842
hari-testpress <hari@testpress.in>
2025-05-17 18:09:21 +0530

    Add tiktoken package

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import tokenizer
+import tiktoken
 
 with open("/Users/testpress/myLLM/the_verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
@@ -7,4 +8,12 @@ tokens = tokenizer.tokenize(raw_text)
 vocabulary = tokenizer.generate_vocabulary(tokens)
 simple_tokenizer = tokenizer.SimpleTokenizerV1(vocabulary)
 
-print(simple_tokenizer.encode("Hello world.<|endoftext|> in the course of world"))
+# print(simple_tokenizer.encode("Hello world.<|endoftext|> in the course of world"))
+
+gpt2_tokenizer = tiktoken.get_encoding("gpt2")
+
+token_ids= gpt2_tokenizer.encode("Hello world, in the course of world")
+print(token_ids, gpt2_tokenizer.decode(token_ids))
+token_ids = gpt2_tokenizer.encode("Hari hara nathan")
+for token_id in token_ids:
+    print(token_id, gpt2_tokenizer.decode([token_id]))
