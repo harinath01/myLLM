790f452bcf114482ceb5c08f020ef2197084e3fb
hari-testpress <hari@testpress.in>
2025-05-17 15:41:01 +0530

    Support encoding/decoding text based on the_verdict vocabulary

--- src/main.py
+++ src/main.py
@@ -4,4 +4,10 @@ with open("/Users/testpress/myLLM/the_verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
 
 tokens = tokenizer.tokenize(raw_text)
-print(tokens)
+vocabulary = tokenizer.generate_vocabulary(tokens)
+simple_tokenizer = tokenizer.SimpleTokenizerV1(vocabulary)
+
+while True:
+    token_ids = input("Enter tokens to decode\n")
+    preprocessed_token_ids = [int(token_id) for token_id in token_ids.split(",")if token_id.strip()]
+    print("Tokenized result: '%s'" %simple_tokenizer.decode(preprocessed_token_ids))
--- src/tokenizer.py
+++ src/tokenizer.py
@@ -6,5 +6,22 @@ def tokenize(raw_text):
 
     return tokens
 
+def generate_vocabulary(tokens):
+    unique_tokens = sorted(set(tokens))
+    
+    return {token:id for id,token in enumerate(unique_tokens) }
 
 
+class SimpleTokenizerV1:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {id: token for id, token in enumerate(vocab)}
+
+    def encode(self, sentence):
+        tokens = tokenize(sentence)
+
+        return [self.str_to_int[token] for token in tokens]
+
+    def decode(self, ids):
+        texts =  [self.int_to_str[id] for id in ids]
+        return " ".join(texts)
