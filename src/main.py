171bec5ce443a1257dc913ddab264559ae24219b
hari-testpress <hari@testpress.in>
2025-05-17 16:06:27 +0530

    Include tokens for unknown vocabulary and end of text

--- src/main.py
+++ src/main.py
@@ -7,7 +7,4 @@ tokens = tokenizer.tokenize(raw_text)
 vocabulary = tokenizer.generate_vocabulary(tokens)
 simple_tokenizer = tokenizer.SimpleTokenizerV1(vocabulary)
 
-while True:
-    token_ids = input("Enter tokens to decode\n")
-    preprocessed_token_ids = [int(token_id) for token_id in token_ids.split(",")if token_id.strip()]
-    print("Tokenized result: '%s'" %simple_tokenizer.decode(preprocessed_token_ids))
+print(simple_tokenizer.encode("Hello world.<|endoftext|> in the course of world"))
--- src/tokenizer.py
+++ src/tokenizer.py
@@ -1,5 +1,9 @@
 import re
 
+
+UNKNOWN_TEXT_TOKEN = '<|unk|>'
+END_OF_TEXT_TOKEN = '<|endoftext|>'
+
 def tokenize(raw_text):
     preprocessed = re.split(r'([,.?_!"()\']|--|\s)', raw_text)
     tokens = [item.strip() for item in preprocessed if item.strip()]
@@ -8,6 +12,7 @@ def tokenize(raw_text):
 
 def generate_vocabulary(tokens):
     unique_tokens = sorted(set(tokens))
+    unique_tokens.extend([UNKNOWN_TEXT_TOKEN, END_OF_TEXT_TOKEN])
     
     return {token:id for id,token in enumerate(unique_tokens) }
 
@@ -20,8 +25,13 @@ class SimpleTokenizerV1:
     def encode(self, sentence):
         tokens = tokenize(sentence)
 
-        return [self.str_to_int[token] for token in tokens]
+        return [self.str_to_int[token] if self._is_part_of_vocab(token) else self.str_to_int[UNKNOWN_TEXT_TOKEN] 
+                for token in tokens]
 
     def decode(self, ids):
         texts =  [self.int_to_str[id] for id in ids]
+        
         return " ".join(texts)
+
+    def _is_part_of_vocab(self, token):
+        return token in self.str_to_int.keys()
